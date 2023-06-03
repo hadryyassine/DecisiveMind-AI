397df90b2f1d36c9ed4598ef175ef87ccd18b627
hadryyassine <hadryyassine@gmail.com>
2023-06-03 07:20:41 +0100

    set up backend project

---
+++ Backend_decisivemind/src/main/java/com/decisivemind/Backend_decisivemind/BackendDecisivemindApplication.java
@@ -0,0 +1,13 @@
+package com.decisivemind.Backend_decisivemind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDecisivemindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDecisivemindApplication.class, args);
+	}
+
+}
---
+++ Backend_decisivemind/src/test/java/com/decisivemind/Backend_decisivemind/BackendDecisivemindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.decisivemind.Backend_decisivemind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDecisivemindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
