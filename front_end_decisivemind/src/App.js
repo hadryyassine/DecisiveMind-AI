570d6c5d922fb7262486b92f7f205a476f5fd8d7
Salma GARMOUTI <101300774+GarmoutiSalma@users.noreply.github.com>
2023-06-07 20:45:36 +0100

    set up of front end

--- front_end_decisivemind/src/App.js
+++ front_end_decisivemind/src/App.js
@@ -7,6 +7,8 @@ import {
 
 import DashboardPage from './pages/Dashboard';
 import ResultPage from './pages/Result';
+
+
 function App() {
   return (
     <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-20 lg:px-8">
