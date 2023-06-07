0dd8f16b0eb32bf89a6f8a6cea76330e3ca20f70
hadryyassine <hadryyassine@gmail.com>
2023-06-07 20:49:12 +0100

    Update of the api

--- Backend_decisivemind/src/main/java/com/decisivemind/Backend_decisivemind/configuaration/ChatGptConfig.java
+++ Backend_decisivemind/src/main/java/com/decisivemind/Backend_decisivemind/configuaration/ChatGptConfig.java
@@ -5,7 +5,7 @@ public class ChatGptConfig {
 
     public static final String AUTHORIZATION = "Authorization";
     public static final String BEARER = "Bearer ";
-    public static final String API_KEY = "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";
+    public static final String API_KEY = "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";
     public static final String MODEL = "text-davinci-003";
     public static final Integer MAX_TOKEN = 300;
     public static final Double TEMPERATURE = 0.0;
